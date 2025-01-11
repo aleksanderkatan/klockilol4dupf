0ed2b39301ab4797cfa86bf7c0cea32b7697834c
Aleksander Katan <aleksander.katan@student.uj.edu.pl>
2025-01-11 14:09:31 +0100

    Refactor block_entrance_random.

--- game/src/blocks/impure/block_entrance_random.py
+++ game/src/blocks/impure/block_entrance_random.py
@@ -21,33 +21,28 @@ class block_entrance_random(block):
 
     def update_target_level(self):
         if self.configuration not in [1, 2, 3, 4]:
-            log.error("Random entrance configuration invalid")
+            log.error("Random entrance configuration invalid.")
             self.target_level = None
             return
 
-        if self.configuration == 1:
-            self.target_level = (101, 0)
-        elif self.configuration == 2:
-            self.target_level = (102, 0)
-        elif self.configuration == 3:
-            self.target_level = (103, 0)
-        elif self.configuration == 4:
-            self.target_level = (104, 0)
+        self.target_level = (100 + self.configuration, 0)
 
     def on_step_in(self):
         # also add above!
         result = False
-        if self.configuration == 1:
-            result = r.generate_LSLG(index=self.target_level, x=8, y=8, ice=0, jump2=0, jump3=0, arrow=0, length=50,
-                                     redirect=5, max_num=3, min_total=30)
-        elif self.configuration == 2:
-            result = r.generate_LSLG(index=self.target_level, x=8, y=8, ice=0, jump2=10, jump3=0, arrow=0, length=50,
-                                     redirect=3, max_num=3, min_total=None)
-        elif self.configuration == 3:
-            result = r.generate_PLG(index=self.target_level, x=7, y=7, portals=4, min_portals=2, pair_portals=True,
-                                    length=30, redirect=4)
-        elif self.configuration == 4:
-            result = r.generate_EULG()
+        match self.configuration:
+            case 1:
+                result = r.generate_LSLG(index=self.target_level, x=8, y=8, ice=0, jump2=0, jump3=0, arrow=0, length=50,
+                                         redirect=5, max_num=3, min_total=30)
+            case 2:
+                result = r.generate_LSLG(index=self.target_level, x=8, y=8, ice=0, jump2=10, jump3=0, arrow=0,
+                                         length=50,
+                                         redirect=3, max_num=3, min_total=None)
+            case 3:
+                result = r.generate_PLG(index=self.target_level, x=7, y=7, portals=4, min_portals=2, pair_portals=True,
+                                        length=30, redirect=4)
+            case 4:
+                result = r.generate_EULG()
 
         if result is None or not result:
             return
